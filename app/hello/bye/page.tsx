8fa5d375e6d9116585591f5aba1e82fe4cbe42a4
Taeik-dot <Taeik-dot@users.noreply.github.com>
2026-03-01 21:08:57 +0900

    Initial commit

---
+++ app/hello/bye/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+        <main>가지마세요ㅠ</main>
+    );
+}
---
+++ app/hello/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+        <main>내용이 너무 유익해요!</main>
+    );
+}
